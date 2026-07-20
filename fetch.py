1c1948aa0476fc069595eae3648bd90955c7cac3
ivadebandit <iva.bakalovska@gmail.com>
2026-07-20 23:05:40 +0200

    add light param to get_session for faster loading

--- fetch.py
+++ fetch.py
@@ -1,14 +1,20 @@
 import fastf1
 import pandas as pd
 
-def get_session(year, location, session_type):
+def get_session(year, location, session_type, light=False):
     session = fastf1.get_session(year, location, session_type)
-    session.load(laps=True, telemetry=True, weather=True)
+
+    if light:
+        session.load(laps=True, telemetry=False, weather=False)
+    else:
+        session.load(laps=True, telemetry=True, weather=True)
+
     return session
 def get_driver_laps(session, driver):
     laps =  session.laps.pick_drivers(driver)
     return laps
- 
+
+  
   
  
  

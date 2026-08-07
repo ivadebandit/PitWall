a15d1d0e8d0d8475f45c905f534dcdc5dd131bcc
ivadebandit <iva.bakalovska@gmail.com>
2026-08-07 19:13:13 +0200

    add get_session and get_driver_laps functions to get raw data

---
+++ fetch.py
@@ -0,0 +1,13 @@
+import fastf1
+import pandas as pd
+
+def get_session( year, location, session_type,  light=False):
+    session =fastf1.get_session(year, location, session_type)
+    if light:
+        session.load(laps=True, telemetry=False, weather=False)
+    else:
+        session.load(laps=True,  telemetry=True, weather=True)
+    return session
+def get_driver_laps(session,driver):
+    laps = session.laps.pick_drivers(driver)
+    return laps
